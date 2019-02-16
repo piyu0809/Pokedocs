c53db2a968422d6a75891eab831160959d276b57
piyu0809 <phirema1@binghamton.edu>
2019-02-15 19:03:05 -0500

    Updates

---
+++ precache-manifest.7f90ae2db8f2b91afa52775c22f4cb74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71ad09f5a16fb2a4e140",
+    "url": "/Pokedocs/static/css/main.a27f75f6.chunk.css"
+  },
+  {
+    "revision": "71ad09f5a16fb2a4e140",
+    "url": "/Pokedocs/static/js/main.71ad09f5.chunk.js"
+  },
+  {
+    "revision": "580f2dda3760b5fd0253",
+    "url": "/Pokedocs/static/js/1.580f2dda.chunk.js"
+  },
+  {
+    "revision": "b0b51b7d833e3bb2371e",
+    "url": "/Pokedocs/static/js/runtime~main.b0b51b7d.js"
+  },
+  {
+    "revision": "cee845c5f941a831101a241800a87636",
+    "url": "/Pokedocs/static/media/poke.cee845c5.jpg"
+  },
+  {
+    "revision": "cdf14907ff062d21558725c260c2b852",
+    "url": "/Pokedocs/index.html"
+  }
+];
